580a913eec9f3ee0d92afd16a653195f1eb210b3
KipyegonB <koechbernard08@gmail.com>
2022-10-09 23:30:58 +0300

    created heading  nad paragraph

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -4,9 +4,9 @@ function ProductsList({ productDescription }) {
     console.log(productDescription)
     const products = productDescription.map((product) => (
         <div className='imageCard'>
-            <img src={product.url} alt="Oops!" />
-            <h3>Type: {product.type}</h3>
-            <h4>Price:{product.price}</h4>
+            <img src={product.Url} alt="Oops!" />
+            <h3>Type: {product.Type}</h3>
+            <h4>Price:{product.Price}</h4>
 
         </div>
     ))
