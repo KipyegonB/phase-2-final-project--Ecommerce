4c3688b0245403a2483c4ec87bf69a610724ae21
KipyegonB <koechbernard08@gmail.com>
2022-10-09 23:24:03 +0300

    created paragraph and provided image

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -4,8 +4,12 @@ import React from "react";
 function Homepage() {
     return (
         <div id="home">
-            <h2>Shop With Us</h2>
-            <p>We Provide first choice for our customers who care about value, innovation and sustainability</p>
+            <h2>
+                Shop With Us
+                </h2>
+            <p>
+                We Provide first choice for our customers who care about value, innovation and sustainability
+            </p>
             <img src="https://media.istockphoto.com/photos/portrait-of-afro-man-in-the-clothing-store-picture-id1185068908?b=1&k=20&m=1185068908&s=170667a&w=0&h=mny8jWyApAvrdsuv35a3wP4wUhWdoFpS4b8q-XLtlr4=" alt="oops" />
         </div>
     );
